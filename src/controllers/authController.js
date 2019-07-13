2ffe1b9a0939f1ddf3c441f6ecdc20d7fe6f6c30
Alessandro L. Santos <aleluizsantos@gmail.com>
2019-07-13 16:58:43 -0300

    Init-Install mongoose, bcryptjs, body-parser

---
+++ src/controllers/authController.js
@@ -0,0 +1,21 @@
+const express = require('express');
+
+const User = require('../models/user');
+
+const router = express.Router();
+
+//Método registar um usuário
+router.post('/register', async (req, res) => {
+    try {
+        const user = await User.create(req.body);
+
+        return res.send({ user })
+
+    } catch (error) {
+        return res.status(400).send( { error: 'Registration failed' } );
+    }
+});
+
+//Recuperar o app passado pelo index.js aplicação principal
+//(app) é o parametro recebido => retornando app.user redefinindo uma ROTA
+module.exports = app => app.use('/auth', router);
---
+++ src/index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+//Referenciando o authController repassando para controller o app 
+//que é nossa aplicação
+require('./controllers/authController')(app);
+
+app.listen(process.env.PORT || 3000, function() {
+    console.log('..:: Servidor online::..')
+});
