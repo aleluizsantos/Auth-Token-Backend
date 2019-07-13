5d9b923ab4f23a6e2e0351239f6386344b69eea0
Alessandro L. Santos <aleluizsantos@gmail.com>
2019-07-13 17:53:22 -0300

    Alteração no authController e user.js

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -6,8 +6,20 @@ const router = express.Router();
 
 //Método registar um usuário
 router.post('/register', async (req, res) => {
+
+    const{ email } = req.body;
+
     try {
+
+        //Procurar se o usuário já existe
+        if(await User.findOne({ email }))
+            return res.status(400).send( { error: 'User already exists' });
+
+        //Criar o usuário passado
         const user = await User.create(req.body);
+
+        //Náo retornar o password informado mesmo criptografado
+        user.password = undefined;
 
         return res.send({ user })
 
