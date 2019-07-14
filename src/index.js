262ef37d6d056256df933e6260bc0b10d2ccae3b
Alessandro L. Santos <aleluizsantos@gmail.com>
2019-07-14 17:21:17 -0300

    Criado index no controller, add todos controller na app

--- src/index.js
+++ src/index.js
@@ -7,9 +7,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 //Referenciando o authController repassando para controller o app 
-//que é nossa aplicação
-require('./app/controllers/authController')(app);
-require('./app/controllers/projetController')(app);
+//que é nossa aplicação index pega todas os nosso controller
+require('./app/controllers/index')(app);
 
 app.listen(process.env.PORT || 3000, function() {
     console.log('..:: Servidor online::..')
