235694cbdc14c1fa7cf6759ce4bfb26c47e09ed1
Alessandro L. Santos <aleluizsantos@gmail.com>
2019-07-14 14:12:18 -0300

    Criado a authenticate sem token

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const bcrypt = require('bcryptjs');
 
 const User = require('../models/user');
 
@@ -27,6 +28,31 @@ router.post('/register', async (req, res) => {
         return res.status(400).send( { error: 'Registration failed' } );
     }
 });
+
+//Metodo para criar um token
+router.post('/authenticate', async (req, res) => {
+    //Quando o usuario se autentica recebo o email e o password
+    const { email, password } = req.body;
+
+    //buscar o usuário para verificar se existe 
+    //Como foi definido no schemaUser que o password seja select= false 
+    //não se exibido, usaremos .select('+password') para exibir o password
+    const user = await User.findOne( { email } ).select('+password'); 
+
+    //Caso o usuario não for encontrado
+    if(!user)
+        return res.status(400).send({ error: 'User not found'});
+
+    //Realizar a verificação se a senha que foi enviada é a mesma que esta cadastrada
+    if(!await bcrypt.compare(password, user.password))
+        return res.status(400).send({ error: 'Invalid password'});
+    
+    //Para não exibir o password na listagem vasmos retira-lo
+    user.password = undefined;
+
+    //Retornar o usuario localizado
+    res.send( { user });
+})
 
 //Recuperar o app passado pelo index.js aplicação principal
 //(app) é o parametro recebido => retornando app.user redefinindo uma ROTA
