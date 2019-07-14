d46db254aa0498187edbb799c7fdf552ccd47acf
Alessandro L. Santos <aleluizsantos@gmail.com>
2019-07-14 16:35:16 -0300

    Criado pasta app e movido controller, models e middleware

--- src/index.js
+++ src/index.js
@@ -8,8 +8,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 //Referenciando o authController repassando para controller o app 
 //que é nossa aplicação
-require('./controllers/authController')(app);
-require('./controllers/projetController')(app);
+require('./app/controllers/authController')(app);
+require('./app/controllers/projetController')(app);
 
 app.listen(process.env.PORT || 3000, function() {
     console.log('..:: Servidor online::..')
